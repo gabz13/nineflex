bd95e9f0ad57ebe29ac37b0faa3b03ef902b7e96
gabrielle simon <gabriellesimon23@gmail.com>
2019-07-21 22:01:30 +0300

    freelancer controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'freelancers/index'
+  get 'freelancers/new'
+  get 'freelancers/show'
+  get 'freelancers/update'
+  get 'freelancers/create'
+  get 'freelancers/edit'
+  get 'freelancers/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
